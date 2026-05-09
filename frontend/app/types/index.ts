f7f2d71c3ff2eb0b7c5f4b0293d712ca8128a8d6
Sunehra Tabassum <sunehrat815@gmail.com>
2026-05-09 20:26:46 +0500

    define timer state interface

--- frontend/app/types/index.ts
+++ frontend/app/types/index.ts
@@ -1,39 +1,79 @@
-interface iUserSignupdata{
-  name:string;
-  email:string;
-  password:string
+interface iUserSignupdata {
+  name: string;
+  email: string;
+  password: string;
 }
 
-interface iUserLogindata{
-email:string;
-password:string
+interface iUserLogindata {
+  email: string;
+  password: string;
 }
 
-interface sessionSetting{
-  focusDuration : number;
-  shortBreakduration :number;
+interface userInfo {
+  userName: string | null;
+  userEmail: string | null;
+  joinedDate: string | null;
+  dailySessionCount: number | null;
+  totalfocusTime: number|null;
+  totalPomodoros:number|null;
+  totalHours:number|null;
+  averagePerday: number|null;
+}
+
+interface sessionSetting {
+  focusDuration: number;
+  shortBreakduration: number;
   longBreakduration: number;
-  sessionCount:number
+  sessionCount: number;
+}
+interface timerState {
+  title: string | null;
+  timerMode: TimerMode;
+  isRunning: boolean;
+  startTime: number | null;
+  endTime: number | null;
+  duration: number; // always stored in MINUTES
+  defaultDuration: number;
 }
 
-interface sessionState{
-  sessionMode: string;
-  title? : string;
-  sessionDuration:Number;
-  completedAt:String;
-  date:Date
+interface sessionState {
+  completedAt: number;
+  duration: number;
+  mode: string;
+  title?: string;
 }
 
-interface settingsSlice{
-    focusTime: number;
-    shortBreak:number;
-    longBreak:number;
-    sessionCount:number
+interface settingsSlice {
+  focusTime: number;
+  shortBreak: number;
+  longBreak: number;
+  sessionCount: number;
 }
+
+type TimerMode = "Focus" | "Short Break" | "Long Break";
+
+interface anaylticsData {
+  date: string;
+  focusTime: number;
+  breakTime: number;
+}
+
+interface analytics {
+  analyticsRange: number  ;
+  bestWeekday:string  | null;
+  averageSessionPerweek:number  | null;
+  totalWeekFocus: number  | null;
+} 
 
 export type {
-    iUserLogindata,
-    iUserSignupdata,
-    sessionSetting,
-    settingsSlice
-}
+  iUserLogindata,
+  iUserSignupdata,
+  sessionSetting,
+  settingsSlice,
+  sessionState,
+  userInfo,
+  TimerMode,
+  analytics,
+  anaylticsData,
+  timerState
+};
